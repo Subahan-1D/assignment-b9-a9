9b3204e0605e7d65c326c10a2c107b54d1f99c34
Subahan-1D <subahanislam523@gmail.com>
2024-04-24 01:17:53 +0600

    added login page

--- src/Shared/Navbar/Navbar.jsx
+++ src/Shared/Navbar/Navbar.jsx
@@ -1,11 +1,11 @@
-import { NavLink } from "react-router-dom";
-
-import logo from '../../../public/main-logo.svg'
+import { Link, NavLink } from "react-router-dom";
+import userDefaultpic from '../../assets/user.png'
 const Navbar = () => {
     const navLinks = <>
         <li><NavLink to='/'>Home</NavLink></li>
         <li><NavLink to='/blog'>Blog</NavLink></li>
         <li><NavLink to='/update'>Update Profile</NavLink></li>
+        <li><NavLink to='/login'>Login</NavLink></li>
     </>
     return (
         <div>
@@ -16,12 +16,12 @@ const Navbar = () => {
                             <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
                         </div>
                         <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-                           {navLinks}
+                            {navLinks}
                         </ul>
                     </div>
                     <div className="flex">
-                        
-                         <NavLink to="/" className="btn btn-ghost text-xl font-playfair-display text-2xl"><span><img className='w-14' src="https://i.ibb.co/Brwdh7B/logo.jpg" alt="" /></span>ALFA Deals</NavLink>
+
+                        <NavLink to="/" className="btn btn-ghost text-xl font-playfair-display text-2xl"><span><img className='w-14' src="https://i.ibb.co/Brwdh7B/logo.jpg" alt="" /></span>ALFA Deals</NavLink>
                     </div>
                 </div>
                 <div className="navbar-center hidden lg:flex">
@@ -30,7 +30,14 @@ const Navbar = () => {
                     </ul>
                 </div>
                 <div className="navbar-end">
-                    <a className="btn">Button</a>
+                    <div tabIndex={0} role="button" className="btn btn-ghost btn-circle avatar">
+                        <div className="w-10   rounded-full">
+                            <img alt="Tailwind CSS Navbar component" src={userDefaultpic} />
+                        </div>
+                    </div>
+                    <Link>
+                        <button className="btn btn-secondary">Login</button>
+                    </Link>
                 </div>
             </div>
         </div>
--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -4,24 +4,29 @@ import Home from "../pages/Home";
 import ErrorElement from "../Layouts/ErrorElement";
 import Blog from "../pages/Blog";
 import Update from "../pages/Update";
+import Login from "../pages/Login";
 
 const router = createBrowserRouter([
     {
-        path:'/',
-        element:<Root></Root>,
-        errorElement:<ErrorElement></ErrorElement>,
-        children:[
+        path: '/',
+        element: <Root></Root>,
+        errorElement: <ErrorElement></ErrorElement>,
+        children: [
             {
-                path:'/',
-                element:<Home></Home>
+                path: '/',
+                element: <Home></Home>
             },
             {
-                path:'blog',
-                element:<Blog></Blog>
+                path: 'blog',
+                element: <Blog></Blog>
             },
             {
-                path:'/update',
-                element:<Update></Update>
+                path: '/update',
+                element: <Update></Update>
+            },
+            {
+                path: '/login',
+                element: <Login></Login>
             }
         ]
     }
